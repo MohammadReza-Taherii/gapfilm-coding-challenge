d475a0e485c3993babcbcef00d35908f3a75bf79
MohammadReza <taheri.mr73@gmail.com>
2022-10-23 14:45:56 +0330

    create header Comp + navbar

--- components/header.js
+++ components/header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import TopNav from "./topNav";
+import Navbar from "./navbar";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className="flex flex-col container mx-auto">
+      <TopNav />
+      <Navbar />
+    </div>
+  );
 };
 
 export default Header;
