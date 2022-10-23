3b97e0fdeb47d91e6ea4ba38d6da2f4e3ddd4de6
MohammadReza <taheri.mr73@gmail.com>
2022-10-23 09:59:04 +0330

    first initial + next js + taiwind + configs

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+import MainPage from "../components/mainPage";
+
+const HomePage = () => {
+  return (
+    <div className="font-Righteous font-righteous">
+      <Header />
+      <MainPage />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
