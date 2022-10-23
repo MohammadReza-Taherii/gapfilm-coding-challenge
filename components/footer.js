f1319480459e4f81b47931a1aa10a9e83c26089d
MohammadReza <taheri.mr73@gmail.com>
2022-10-23 14:45:00 +0330

    create Footer component

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className="flex bg-black h-12 items-center">
+      <span className="font-sans text-sm text-white text-opacity-50 container mx-auto">
+        Celebrities fan zone © 2019 Privacy Policy
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
